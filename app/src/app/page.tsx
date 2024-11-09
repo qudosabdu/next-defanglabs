3ccfc27475c0536773bc5c5096e7bdbd64906a2b
Abdul Qudoos <80036635+qudosabdu@users.noreply.github.com>
2024-11-09 20:46:31 +0500

    This is first commit

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
-      <h1 className="text-4xl font-bold text-white">Next.js &times; Defang</h1>
+      <h1 className="text-4xl font-bold text-white">This is my demo project using DefangLabs</h1>
     </main>
   );
 }
